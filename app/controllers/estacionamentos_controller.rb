18ae79737930760d8c53a21bbb23ae96901bb29c
Luiz Davi <luizd4398@gmail.com>
2022-04-27 17:09:39 -0300

    ajustando rotas de estacionamento

--- app/controllers/estacionamentos_controller.rb
+++ app/controllers/estacionamentos_controller.rb
@@ -1,32 +1,10 @@
 class EstacionamentosController < ApplicationController
-  before_action :set_estacionamento, only: %i[ show edit update destroy ]
-
-  def index
-    @estacionamentos = Estacionamento.all
-  end
+  before_action :set_estacionamento, only: %i[ show edit update ]
 
   def show
   end
 
-  def new
-    @estacionamento = Estacionamento.new
-  end
-
   def edit
-  end
-
-  def create
-    @estacionamento = Estacionamento.new(estacionamento_params)
-
-    respond_to do |format|
-      if @estacionamento.save
-        format.html { redirect_to estacionamento_url(@estacionamento), notice: "Estacionamento was successfully created." }
-        format.json { render :show, status: :created, location: @estacionamento }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @estacionamento.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def update
@@ -38,15 +16,6 @@ class EstacionamentosController < ApplicationController
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @estacionamento.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  def destroy
-    @estacionamento.destroy
-
-    respond_to do |format|
-      format.html { redirect_to estacionamentos_url, notice: "Estacionamento was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
